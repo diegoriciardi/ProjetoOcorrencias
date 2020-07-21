84693dbea1dac359fb794148209df62b182629e0
professorisidro <isidro@professorisidro.com.br>
2020-07-21 10:58:09 -0300

    novas funcionalidades

---
+++ src/main/java/br/com/isidrocorp/projetofinal/controller/DepartamentoController.java
@@ -0,0 +1,25 @@
+package br.com.isidrocorp.projetofinal.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.isidrocorp.projetofinal.dao.DepartamentoDAO;
+import br.com.isidrocorp.projetofinal.model.Departamento;
+
+@RestController
+public class DepartamentoController {
+	
+	@Autowired
+	DepartamentoDAO dao;
+	
+	@GetMapping("/departamentos")
+	public ArrayList<Departamento> recuperarTodos(){
+		ArrayList<Departamento> lista;
+		lista = (ArrayList<Departamento>)dao.findAll();
+		return lista;
+	}
+
+}
